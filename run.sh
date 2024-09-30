fbc0681be425845d3eb5ea1b29b838ad2f57f1ec
Hamish B <hmblair@stanford.edu>
2024-09-30 12:28:09 -0700

    For running on Sherlock

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=bash
+#SBATCH --time=04:00:00
+#SBATCH --partition=rhiju
+#SBATCH --gres=gpu:0
+#SBATCH --cpus-per-task=8 
+#SBATCH --mem=32G
+
+ml load python/3.12.1
+
+pip install -r requirements.txt
+
+...
