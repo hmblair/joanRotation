4d2356a656e341eab983b302e2413e06148423da
Hamish B <hmblair@stanford.edu>
2024-09-30 13:27:53 -0700

    Helper to time epoch times

---
+++ timer.py
@@ -0,0 +1,26 @@
+
+from __future__ import annotations
+import torch
+
+
+class EpochTimer:
+    """
+    Helper code to time an epoch of training.
+    """
+
+    def __init__(
+        self: EpochTimer,
+    ) -> None:
+
+        self.start = torch.cuda.Event(enable_timing=True)
+        self.end = torch.cuda.Event(enable_timing=True)
+
+        self.start.record()
+
+    def finish(self: EpochTimer) -> float:
+        """
+        Finish timing the code.
+        """
+        torch.cuda.synchronize()
+        t = self.start.elapsed_time(self.end)
+        return t / 1000
