f87e6af20941a61010a1b8f02828c949c0672706
joantlee <joanlee@stanford.edu>
2024-10-25 10:37:49 -0700

    fixed bugs

--- extract_energies.py
+++ extract_energies.py
@@ -1,15 +1,22 @@
+import os
+import xarray as xr
 from parse import parse_xyz
+from tqdm import tqdm
+import numpy as np
 
 # Get the path that this index corresponds to
-path = 'QM9_dataset'
+paths = []
+folder = 'QM9_data'
 for file in os.listdir(folder):
             if file.endswith('xyz'):
-                self.paths.append(folder + '/' + file)
+                paths.append(folder + '/' + file)
 # Load the coordinates and elements of the atom
-coordinates, elements, energy, charges = parse_xyz(path)
-# Convert the energy to a PyTorch tensor
-energy_arr = np.array(energy) 
-ds = xarray.Dataset({‘energy’: ([‘molecule’], energy_arr)})
-ds.to_netcdf(‘energy.nc’)
-xr.load_dataset(‘energy.nc’)
-
+energy_list = []
+for path in tqdm(paths):
+    coordinates, elements, energy, charges = parse_xyz(path)
+    energy_list.append(energy)
+energy_arr = np.array(energy_list).astype(np.float32) 
+ds = xr.Dataset({"energy": (["molecule"], energy_arr)})
+ds.to_netcdf("energy.nc")
+ds2 = xr.load_dataset("energy.nc")
+#breakpoint()
